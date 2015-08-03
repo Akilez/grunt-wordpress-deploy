2aca73216d803ade5fbea8cc42e404ebe6e9170c
Michael Christensen <mchristensen@themonastery.org>
2015-08-03 16:04:59 -0700

    Fix for warnings on push

--- tasks/lib/util.js
+++ tasks/lib/util.js
@@ -87,6 +87,7 @@ exports.init = function (grunt) {
     var content = grunt.file.read(file);
 
     var output = exports.replace_urls(old_url, new_url, content);
+    var output = preg_replace(/(Warning)/, "-- $1"); // Hack to prevent warnings causing errors
 
     grunt.file.write(file, output);
   };
